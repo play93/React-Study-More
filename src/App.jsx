5ccc6d7f980f9659fe8a8a6ff43125dbb98291be
play93 <95739833+play93@users.noreply.github.com>
2024-08-15 20:00:29 +0900

    project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ color: "blue" }}>Hellow World</h1>
+      <p className="sample">This is a simple app</p>
+    </>
+  );
+}
+
+export default App;
